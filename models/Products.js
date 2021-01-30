2f54322c4328bc356ad3b9d58dde0c97318a3e70
MD <moctardiaw2011@gmail.com>
2021-01-30 22:08:30 +0100

    Use linter

--- models/Products.js
+++ models/Products.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
 
 const ProductSchema = new mongoose.Schema({
-    name : String,
-    type : String,
-    price : Number,
-    rating : Number,
-    warranty_years : Number,
-    available : Boolean
+	name : String,
+	type : String,
+	price : Number,
+	rating : Number,
+	warranty_years : Number,
+	available : Boolean
 })
 
 module.exports = mongoose.model('Product', ProductSchema)
--- routes/Products.js
+++ routes/Products.js
@@ -4,65 +4,77 @@ const Product=require('../models/Products')
 
 // Get all routes
 router.get('/', async (req,res) => {
-    const products = await Product.find()
+	const products = await Product.find()
 
-    res.json(products)
+	res.json(products)
 })
 
 // Create new product
 router.post('/new', async (req,res) => {
 
-    try{
-        const newProduct = new Product(req.body)
-        const savedProduct = await newProduct.save()
+	try{
+		const newProduct = new Product(req.body)
+		const savedProduct = await newProduct.save()
 
-        res.json(savedProduct);
+		res.json(savedProduct)
 
-    } catch (err) {
-        console.log(err)
-        throw err
-    }
+	} catch (err) {
+		console.log(err)
+		throw err
+	} finally{
+		res.status(400)
+		res.json({'error': 'Bad request'})
+	}
 
 })
 
 // Get specific product
 router.get('/:id', async (req, res) =>{
 
-    try{
-        const product = await Product.findById({ _id: req.params.id })
+	try{
+		const product = await Product.findById({ _id: req.params.id })
 
-        res.json(product)
+		res.json(product)
 
-    } catch (err) {
-        console.log(err)
-        throw err
-    }
+	} catch (err) {
+		console.log(err)
+		throw err
+	} finally{
+		res.status(400)
+		res.json({'error': 'Id not found'})
+	}
 })
 
 // Delete a product
 router.delete('/:id', async (req, res) => {
 
-    try{
-        const result = await Product.findByIdAndDelete({ _id: req.params.id })
+	try{
+		const result = await Product.findByIdAndDelete({ _id: req.params.id })
 
-        res.json(result)
-    } catch (err) {
-        console.log(err)
-        throw err
-    }
+		res.json(result)
+	} catch (err) {
+		console.log(err)
+		throw err
+	} finally{
+		res.status(400)
+		res.json({'error': 'Id not found'})
+	}
 })
 
 // Update a product
 router.patch('/:id', async (req, res) => {
 
-    try{
-    const product = await Product.updateOne({_id: req.params.id}, {$set: req.body})
+	try{
+		const product = await Product.updateOne({_id: req.params.id}, {$set: req.body})
 
-    res.json(product)
-    } catch (err) {
-        console.log(err)
-        throw err
-    }
+		res.json(product)
+	} catch (err) {
+		console.log(err)
+		throw err
+	} finally{
+		res.status(400)
+		res.json({'error': 'Id not found'})
+	}
 })
 
 module.exports = router
